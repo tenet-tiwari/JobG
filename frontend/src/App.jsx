745df696d199f332ef0060da9347ef8d935589eb
samurai beta <tenet.tiwari2003@gmail.com>
2024-08-01 00:48:29 +0530

    changed backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://jobg.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
